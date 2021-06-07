08da6cf4b527cdf18da6baf63f7a80daed542bbf
Gilbert Lagos <gilbert.ata@gmail.com>
2021-06-07 11:14:15 -0400

    Update settings.py

--- rental_pucon/settings.py
+++ rental_pucon/settings.py
@@ -16,6 +16,7 @@ import environ
 # environ init 
 env = environ.Env()
 environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,13 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-k0^(-uulsdvvr%fj)p@*azebx)jd3ue-_m74^o1cgu5w2qzlz^'
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG',default=False)
 
-#ALLOWED_HOSTS = []
 ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS',default=[]))
 
 # Application definition
